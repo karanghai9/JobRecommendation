2005f91f16287753bc20a27998eb594b137f6793
karanghai9 <37792167+karanghai9@users.noreply.github.com>
2024-12-01 21:18:10 +0100

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -16,7 +16,7 @@ from webdriver_manager.core.os_manager import ChromeType
 # from webdriver_manager.chrome import ChromeDriverManager
 # from webdriver_manager.core.os_manager import ChromeType
 from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as EC
 # from selenium.webdriver.common.by import By
 
 
