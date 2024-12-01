0f168e68d7c78846d3fddad73b3b23963f15540a
karanghai9 <37792167+karanghai9@users.noreply.github.com>
2024-12-01 21:37:15 +0100

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -68,7 +68,7 @@ def main():
             
             options = Options()
             options.add_argument("--disable-gpu")
-            # options.add_argument("--headless")
+            options.add_argument("--headless")
             options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5615.138 Safari/537.36")
             options.add_argument("--disable-extensions")  # Disable any extensions
             options.add_argument("--no-sandbox")  # For certain systems like Linux
