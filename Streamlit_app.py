5906da996b05938f2c491b1b80c90b26f03b24dd
karanghai9 <37792167+karanghai9@users.noreply.github.com>
2024-12-02 01:20:05 +0100

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -110,15 +110,15 @@ def scrapeJobsData(applicantSkills, applicantLocation):
 
             
             st.code(driver.current_url)
-            # st.code(driver.page_source)
+            st.code(driver.page_source)
 
 
 
-            try:
-                response = requests.get("https://www.google.com", verify=False)
-                st.write(f"Response Status: Done")
-            except Exception as e:
-                st.write(f"Error accessing URL: {e}")
+            # try:
+            #     response = requests.get("https://www.google.com", verify=False)
+            #     st.write(f"Response Status: Done")
+            # except Exception as e:
+            #     st.write(f"Error accessing URL: {e}")
 
 
             # try:
@@ -127,22 +127,22 @@ def scrapeJobsData(applicantSkills, applicantLocation):
             # except Exception as e:
             #     st.write(f"Error accessing URL: {e}")
 
-            try:
-                WebDriverWait(driver, 10).until(
-                    EC.presence_of_element_located((By.CLASS_NAME, "res-nehv70"))
-                )
-                divs = driver.find_elements(By.XPATH, "//div[contains(@class, 'res-nehv70')]")
-            except TimeoutException:
-                st.write("Divs with class 'res-nehv70' did not load in time.")
+            # try:
+            #     WebDriverWait(driver, 10).until(
+            #         EC.presence_of_element_located((By.CLASS_NAME, "res-nehv70"))
+            #     )
+            #     divs = driver.find_elements(By.XPATH, "//div[contains(@class, 'res-nehv70')]")
+            # except TimeoutException:
+            #     st.write("Divs with class 'res-nehv70' did not load in time.")
                 
             # divs = driver.find_elements(By.CLASS_NAME, "res-nehv70")
 
             # Get the first div's HTML
-            if divs:
-                div_html = divs[0].get_attribute("outerHTML")
-                st.write(div_html)
-            else:
-                st.write("No div found with class 'res-nehv70'")
+            # if divs:
+            #     div_html = divs[0].get_attribute("outerHTML")
+            #     st.write(div_html)
+            # else:
+            #     st.write("No div found with class 'res-nehv70'")
     
             # divs = divs[:1]
             
