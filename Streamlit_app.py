b0f9680fb4819827f181b8868dcc062f6dc23827
karanghai9 <37792167+karanghai9@users.noreply.github.com>
2024-12-02 04:53:37 +0100

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -102,7 +102,7 @@ def scrapeJobsData(applicantSkills, applicantLocation):
             # else:
             #     st.write("No div found with class 'res-nehv70'")
 
-            st.write(len(divs))
+            # st.write(len(divs))
             # divs = divs[:6]
             
             # Store the URLs to track if the page is already opened
